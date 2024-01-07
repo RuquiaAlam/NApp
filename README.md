a5d95341320c80220d58ee666078368efa37fcd5
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-01-07 13:10:09 -0600

    validation&&redux&&auth

--- README.md
+++ README.md
@@ -6,9 +6,23 @@
 -Routing of App
 -Login Form
 -Sign Up from
-
 - Form Validation
-  -UseRef Hook
-  -Firebase SetUp
-  -Deploying our App
-  -Create Sign Up in Firebase User Account
+-UseRef Hook
+-Firebase SetUp
+-Deploying our App
+-Create Sign Up in Firebase User Account
+-Created Redux Store with userSlice
+-Implemented SignOut
+-Update Profile
+-Movies frrom TMDM movies
+
+#Features
+-Login/SignUp
+-Sign In /Sign up forms
+-Redirect to browse page
+-Browse (after authentication)
+-header
+-Main Movie
+  -Trailer in Background
+  -Title &Description
+  
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,20 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header';
 import checkValidData from "../utils/validate";
-import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import {auth} from "../utils/firebase" ;
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
 const [isSignInForm,setIsSignInForm]=useState(true);
 const [errorMessage,setErrorMessage]=useState(null);
 
 const email=useRef(null);
+const name=useRef(null);
+const navigate=useNavigate();
+const dispatch=useDispatch();
 const password=useRef(null)
 const handleButtonClick=()=>
 {
@@ -28,7 +34,25 @@ if(!isSignInForm)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
+    updateProfile(user, {
+      displayName: name.current.value,photoURL: "https://avatars.githubusercontent.com/u/114290800?v=4"
+    }).then(() => {
+      // Profile updated!
+      const {uid ,email,displayName,photoURL}= auth.currentUser;
+      dispatch(
+        addUser({
+          uid:uid,
+          email:email,
+          displayName:displayName,
+          photoURL:photoURL})
+          );
+      navigate("/browse")
+    }).catch((error) => {
+      // An error occurred
+      setErrorMessage(error.message);
+    });
   console.log(user);
+ 
 
   })
   .catch((error) => {
@@ -47,6 +71,7 @@ signInWithEmailAndPassword(auth, email.current.value,password.current.value)
     const user = userCredential.user;
     // ...
     console.log(user)
+    navigate("/browse");
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -70,7 +95,7 @@ setIsSignInForm(!isSignInForm);
     <form onSubmit={(e)=>e.preventDefault()} className="w-4/12 absolute p-12 my-36 mx-auto right-0 left-0 bg-black text-white rounded-lg  bg-opacity-80">
 <h1 className="font-bold text-3xl p-2 mx-0">{isSignInForm?"Sign In":"Sign Up"}</h1>
 {!isSignInForm&&
-<input type="text" placeholder="Full Name " 
+<input ref={name} type="text" placeholder="Full Name " 
 className="p-4 my-4 w-full bg-gray-600 text-white "></input>}
       <input ref={email} type="text" placeholder="Email Address " className="p-4 my-4 w-full bg-gray-600 text-white "></input>
       <input ref={password}type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-600 text-white" ></input>
